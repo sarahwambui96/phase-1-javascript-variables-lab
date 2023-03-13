20f21d7a314f724e77221c306c92dfb8861040cc
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-13 12:03:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+
+//is defined as a const
+const companyName = "Scuber"
+
+
+//is defined using let
+ let mostProfitableNeighborhood = "Chelsea"
+
+ //is defined using let
+ let companyCeo = "Susan Smith"
